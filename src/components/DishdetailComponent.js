59269bc3d5a62757c99469f8878d34fc6d4cf641
Sheyar <ali.sheyar@gmail.com>
2018-12-14 10:19:53 +0100

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbIte
         Modal, Button, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import { Control, LocalForm, Errors } from 'react-redux-form'
+import { Loading } from './LoadingComponent'
 
 
 const required = (val) => val && val.length
@@ -138,7 +139,23 @@ class CommentForm extends Component {
   }
 
   const DishDetail = (props) => {
-    if(props.dish){
+    if (props.isLoading) {
+      return(
+        <div className="container">
+          <div className="row">
+            <Loading />
+          </div>
+        </div>
+      )
+    } else if (props.errMess) {
+      return(
+        <div className="container">
+          <div className="row">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      )
+    } else if(props.dish){
       return (
         <div className="container">
           <div className="row">
